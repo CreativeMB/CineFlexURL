743fec7243ddcef51c9ce36da8d68fd8c0978fe3
TobiasM <ingcpu@gmail.com>
2024-09-26 11:16:21 -0500

    unicio 3

--- app/src/main/java/com/creativem/cineflexurl/MainActivity.kt
+++ app/src/main/java/com/creativem/cineflexurl/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.creativem.cineflexurl
-
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,8 +19,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var imageUrlEditText: EditText
     private lateinit var streamUrlEditText: EditText
     private lateinit var uploadText: TextView
+    private lateinit var openWebsiteTextView: TextView
 
     private val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,6 +33,7 @@ class MainActivity : AppCompatActivity() {
         imageUrlEditText = findViewById(R.id.imageUrlEditText)
         streamUrlEditText = findViewById(R.id.streamUrlEditText)
         uploadText = findViewById(R.id.uploadText)
+        openWebsiteTextView = findViewById(R.id.url)
 
         uploadText.setOnClickListener {
             val title = titleEditText.text.toString().trim()
@@ -38,9 +43,24 @@ class MainActivity : AppCompatActivity() {
 
             if (title.isEmpty() || synopsis.isEmpty() || imageUrl.isEmpty() || streamUrl.isEmpty()) {
                 Toast.makeText(this, "Todos los campos son obligatorios", Toast.LENGTH_SHORT).show()
+            } else if (!isValidUrl(imageUrl) || !isValidUrl(streamUrl)) {
+                Toast.makeText(this, "Las URLs no son válidas", Toast.LENGTH_SHORT).show()
             } else {
                 uploadMovie(title, synopsis, imageUrl, streamUrl)
             }
+        }
+
+        openWebsiteTextView.setOnClickListener {
+            openWebPage("https://castr.com/hlsplayer/")
+        }
+    }
+
+    private fun isValidUrl(url: String): Boolean {
+        return try {
+            Uri.parse(url)
+            true
+        } catch (e: Exception) {
+            false
         }
     }
 
@@ -60,12 +80,11 @@ class MainActivity : AppCompatActivity() {
         db.collection("movies")
             .add(movie)
             .addOnSuccessListener {
-                Toast.makeText(this, "Datos enviados, Abre Cine Flex en tu TV para ver Contenido", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Película subida correctamente", Toast.LENGTH_LONG).show()
                 clearFields()
             }
             .addOnFailureListener { e ->
-                Toast.makeText(this, "Error al enviar los datos: ${e.message}", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this, "Error al enviar los datos: ${e.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
@@ -74,5 +93,20 @@ class MainActivity : AppCompatActivity() {
         synopsisEditText.text.clear()
         imageUrlEditText.text.clear()
         streamUrlEditText.text.clear()
+    }
+
+    private fun openWebPage(url: String) {
+        Log.d("MainActivity", "Intentando abrir la URL: $url") // Para depuración
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+
+        // Agregando un verificador para asegurarnos que existe un paquete que pueda manejar el intent
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, "No se puede abrir la página: ${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
